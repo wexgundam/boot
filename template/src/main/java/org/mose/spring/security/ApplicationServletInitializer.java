750a1f3a4475ee774609f9b552dc3cffe5b1870b
wexgundam <wexgundam@163.com>
2017-08-01 23:09:55 +0800

    配置spring boot

--- template/src/main/java/org/mose/spring/security/ApplicationServletInitializer.java
+++ template/src/main/java/org/mose/spring/security/ApplicationServletInitializer.java
@@ -3,6 +3,9 @@ package org.mose.spring.security;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 /**
  * 以war包形式部署到web容器的启动类
  */
@@ -10,6 +13,14 @@ public class ApplicationServletInitializer extends SpringBootServletInitializer 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ApplicationInitializer.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+
+        // Servlet context init parameters
+        servletContext.setInitParameter("webAppRootKey", "template.root");
     }
 }
 
